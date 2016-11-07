b53f7f942b96bb4e1b24c48afbeca01f0d8d616c
pathologicalhandwaving <kshort@iastate.edu>
2016-11-06 19:00:04 -0800

    added mplayer cmus

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,4 @@
+/Users/Em/Music/AmbientSpaceMusic.mp3
+/Users/Em/Music/StarTrek-TNGBridgeBackgroundAmbience.mp3
+/Users/Em/Music/StarTrek-TNGSleepingQuarters.mp3
+/Users/Em/Music/iTunes/iTunes Media/Music/Unknown Artist/Unknown Album/Star Trek - TNG Bridge Background Ambience _8 HOURS-UKBvaOLDem0.mp3
