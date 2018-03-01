913598cdeeea7c3149377182c5c27c1339b7246e
pathologicalhandwaving <kshort@iastate.edu>
2018-03-01 15:55:03 -0500

    oni config

---
+++ .oni/config.js
@@ -0,0 +1,41 @@
+// For more information on customizing Oni,
+// check out our wiki page:
+// https://github.com/onivim/oni/wiki/Configuration
+
+const activate = oni => {
+    console.log("config activated")
+
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+}
+
+const deactivate = () => {
+    console.log("config deactivated")
+}
+
+module.exports = {
+    activate,
+    deactivate,
+    //add custom config here, such as
+
+    "ui.colorscheme": "nord",
+
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    //"oni.loadInitVim": false,
+    "editor.fontSize": "11px",
+    //"editor.fontFamily": "FiraCode"
+
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+}
+
