fc9dce0e848c33c5e695d6fadbbb79aa8c9eac4c
EmD <kmshort@protonmail.com>
2018-03-16 23:47:51 -0400

    rm scratch replaced dnote
    TODO: Move aliases
    TODO: Merge with functions
    TODO: Merge external scripts

--- .myscripts/library.sh
+++ .myscripts/library.sh
@@ -311,12 +311,6 @@ function encode() {
 }
 
 
-# Look Busy ;)
-#function busy() {
-#    j=0;while true; do let j=$j+1; for i in $(seq 0 20 100); do echo $i;sleep 1; done | dialog --gauge "Install part $j : `sed $(perl -e "print int rand(99999)")"q;d" /usr/share/dict/words`" 6 40;done
-#}
-#alias busy="busy"
-
 
 # Count number of files in directory
 function numfiles() { 
@@ -324,9 +318,4 @@ function numfiles() {
     echo "$N files in $1";
 }
 
-# New Scratch File and Open
-function scratch() {
-    cd /Users/Em/Repos/ScratchPad/
-    touch $1
-    vim $1
-}
+
