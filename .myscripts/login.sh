e95c0b4485e5f2c3b5adfe35fa1754ab89fb698b
EmD <kmshort@protonmail.com>
2018-03-23 19:07:27 -0400

    login script

---
+++ .myscripts/login.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+###
+# ---
+# title: login.sh
+# description: Terminal login script
+# date-created: 2018-03-23
+# date-modified: 2018-03-23
+# ---
+###
+
+# On system wake up
+# Open space with full screen terminal
+# config pull
+# get last three modified files
+# run doing last
+# show todo list
+# show calendar
+# run motd
+# launch menu
+# wait for user input
+# follow input
