b50d2d928c8a19581e8e4a9fe4d28b22d3e81618
pathologicalhandwaving <kshort@iastate.edu>
2018-03-25 12:52:02 -0400

    update

--- .myscripts/adto.sh
+++ .myscripts/adto.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
 
-FILE="/Users/Em/Repos/list/todo.md"
+FILE="todo.md"
+FPATH="/Users/Em/Repos/list/"
 
 read -p "Add Task: " thing
 
-apndd="- [ ] $thing @created($(date +%Y%m%dT%H%M))"
-echo "$apndd" >> $FILE
+apndd="- [ ] $thing"
+added="@created($(date +%Y%m%dT%H%M))"
+echo "$apndd" >> $FPATH$FILE
+echo "$added" >> $FPATH$FILE
+
+cd $FPATH
+git pull
 clear
-tail -n 1 $FILE
+git add .
+git commit -m "updated list after addition"
+git push
+clear
+
+git show
+read -r -p "Press ENTER to continue"
+clear
+cd
+echo " "
+echo " "
+echo " "
+grc tail -n 2 $FPATH$FILE
 
 exit 0
