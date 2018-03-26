1d89492658a7ef8bff3ee17aca8c09d8b6dd4a2e
pathologicalhandwaving <kshort@iastate.edu>
2018-03-25 22:15:01 -0400

    yay fireworks

--- .myscripts/addone.sh
+++ .myscripts/addone.sh
@@ -4,28 +4,48 @@
 FILE="todo.md"
 FPATH="/Users/Em/Repos/list/"
 
-read -p "Add Finished Task: " thing
+echo
+echo
+echo "💥 🎌 "
+echo "Add Completed Task:" | lolcat
+echo " 🎊 🎏 "
+read -e thing
+echo
+echo
 
-apndd="- [ ] $thing"
-added="@done($(date +%Y%m%dT%H%M))"
+apndd="- [X] $thing"
+finished="@done($(date +%Y%m%dT%H%M))"
+added="@created($(date +%Y%m%dT%H%M))"
 echo "$apndd" >> $FPATH$FILE
-echo "$added" >> $FPATH$FILE
+echo "        $added" >> $FPATH$FILE
+echo "        $finished" >> $FPATH$FILE
 
 cd $FPATH
 git pull
+
 clear
+
 git add .
 git commit -m "updated list after addition of completed task"
 git push
+
 clear
 
 git show
-read -r -p "Press ENTER to continue"
+read -r -p "Press ENTER to continue" | lolcat
+
+
 clear
+
 cd
-echo " "
-echo " "
-echo " "
-grc tail -n 2 $FPATH$FILE
+
+echo
+echo
+echo
+echo
+echo
+echo
+
+tail -n 3 $FPATH$FILE | lolcat
 
 exit 0
--- .myscripts/adto.sh
+++ .myscripts/adto.sh
@@ -4,28 +4,45 @@
 FILE="todo.md"
 FPATH="/Users/Em/Repos/list/"
 
-read -p "Add Task: " thing
-
+echo
+echo
+echo
+echo "🐙  Add Task: 🐙 "  | lolcat
+echo
+read -e thing
+echo
+echo
 apndd="- [ ] $thing"
 added="@created($(date +%Y%m%dT%H%M))"
 echo "$apndd" >> $FPATH$FILE
-echo "$added" >> $FPATH$FILE
+echo "        $added" >> $FPATH$FILE
 
 cd $FPATH
 git pull
+
 clear
+
 git add .
 git commit -m "updated list after addition"
 git push
+
 clear
 
 git show
 read -r -p "Press ENTER to continue"
-clear
-cd
-echo " "
-echo " "
-echo " "
-grc tail -n 2 $FPATH$FILE
 
+clear
+
+cd
+
+echo
+echo
+echo
+echo
+echo
+echo "⚡️  "
+echo "⚡️ "
+tail -n 2 $FPATH$FILE | lolcat
+echo "⚡️ "
+echo "⚡️ "
 exit 0
