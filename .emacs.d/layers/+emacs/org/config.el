858109e6969b82d078bffae23f4d6722306daaca
EmD <kmshort@protonmail.com>
2018-03-31 02:28:24 -0400

    update layers

--- .emacs.d/layers/+emacs/org/config.el
+++ .emacs.d/layers/+emacs/org/config.el
@@ -14,13 +14,13 @@
 (defvar org-enable-bootstrap-support nil
   "If non-nil Twitter Bootstrap related packages are configured.")
 
-(defvar org-enable-github-support nil
+(defvar org-enable-github-support t
   "If non-nil Github related packages are configured.")
 
 (defvar org-enable-reveal-js-support nil
   "If non-nil, enable export to reveal.js.")
 
-(defvar org-projectile-file "TODOs.org"
+(defvar org-projectile-file "~/Repos/tasks/OrgMode/captainslog/todos.org"
   "The file to store project TODOs in. If this is a relative
 path, one file per project is used (and the path is relative to
 the project root). If it an absolute path, one global file is
