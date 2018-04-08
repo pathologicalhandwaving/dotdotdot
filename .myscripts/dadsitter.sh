109eb17989a1a128d0e9ad928b3a6c89335a3b06
pathologicalhandwaving <kshort@iastate.edu>
2018-04-07 23:36:19 -0400

    lib

---
+++ .myscripts/dadsitter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE=/Users/Em/Repos/DadSitting/data/dadevents.json
+
+eventday="date +%Y%m%d"
+eventtime="date +%H:%M"
+
+read -p "Action: " action
+read -p "Where: " place
+read -p "Description: " desc
+
+
