c03aaa1e843b10f66ece673b00a9a88f557eefc7
EmD <kmshort@protonmail.com>
2018-04-17 00:02:50 -0400

    Updated

--- .emacs.d/personal/space-settings.el
+++ .emacs.d/personal/space-settings.el
@@ -9,6 +9,35 @@
 ;; org-brain
 (prelude-require-package 'org-brain)
   
+;; PDF Reading Setup
+
+;; pdf-tools
+(prelude-require-package 'pdf-tools)
+(pdf-tools-install)
+;; Scale page to fit
+(setq-default pdf-view-display-size 'fit-page)
+;; Automatically annotate highlights
+(setq pdf-annot-activate-created-annotations t)
+(define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
+;; Turn off cua so copy works
+(add-hook 'pdf-view-mode-hook (lambda () (cua-mode 0)))
+;; Fine-grained zooming
+(setq pdf-view-resize-factor 1.1)
+;; Keyboard shortcuts
+(define-key pdf-view-mode-map (kbd "h") 'pdf-annot-add-highlight-markup-annotation)
+(define-key pdf-view-mode-map (kbd "t") 'pdf-annot-add-text-annotation)
+(define-key pdf-view-mode-map (kbd "D") 'pdf-annot-delete)
+
+;; org-ref
+(prelude-require-package 'org-ref)
+(setq org-ref-bibliography-notes "/Users/Em/Repos/ImInSpace/Library/Refs/notes.org"
+      org-ref-default-bibliography '("/Users/Em/Repos/ImInSpace/Library/Refs/default.bib")
+      org-ref-pdf-directory "/Users/Em/Repos/ImInSpace/Library/Refs/PDFs/")
+;; helm-bibtex
+(setq bibtex-completion-bibliography "/Users/Em/Repos/ImInSpace/Library/Refs/default.bib")
+      bibtex-completion-library-path "/Users/Em/Repos/ImInSpace/Library/Refs/PDFs/"
+      bibtex-completion-notes-path "/Users/Em/Repos/ImInSpace/Library/Refs/notes.org") 
+
 
   
 (provide 'space-settings)
