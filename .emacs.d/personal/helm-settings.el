835f89efd4c6d1fcdb15c8ee5323d3c247a1d7ce
pathologicalhandwaving <kshort@iastate.edu>
2018-10-18 13:51:18 -0400

    Removed unused functions in image-settings and helm-settings

--- .emacs.d/personal/helm-settings.el
+++ .emacs.d/personal/helm-settings.el
@@ -5,9 +5,9 @@
 
 ;;; helm-frame
 
-(add-hook 'helm-after-action-hook 'helm-frame-delete)
-(add-hook 'helm-cleanup-hook 'helm-frame-delete)
-(setq helm-split-window-preferred-function 'helm-frame-window)
+
+
+
 
 
 
--- .emacs.d/personal/image-settings.el
+++ .emacs.d/personal/image-settings.el
@@ -1,13 +1,11 @@
 ;;; image-settings.el --- Image Settings
 
 ;;; Code:
-(setq org-image-actual-width nil)
 
-(defun do-org-show-all-inline-images ()
-  (interactive)
-  (org-display-inline-images t t))
 
-(global-set-key (kbd "C-c C-x C v") 'do-org-show-all-inline-images)
+
+
+
 
 ;;; End:
 
